9993672321d6e92a5b42b14c7a6252d482c437d0
Samuel Abreu <54046223+Abrolhus@users.noreply.github.com>
2023-02-07 16:17:25 -0300

    packer compiled

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -134,6 +134,11 @@ _G.packer_plugins = {
     path = "/home/abrolhus/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
+  ["lsp_signature.nvim"] = {
+    loaded = true,
+    path = "/home/abrolhus/.local/share/nvim/site/pack/packer/start/lsp_signature.nvim",
+    url = "https://github.com/ray-x/lsp_signature.nvim"
+  },
   ["lspkind.nvim"] = {
     loaded = true,
     path = "/home/abrolhus/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
