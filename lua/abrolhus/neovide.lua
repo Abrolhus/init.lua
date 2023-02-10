0ba3cd3909ba5e8aed92530bc4f5d67a59e1fe1b
Samuel Abreu <54046223+Abrolhus@users.noreply.github.com>
2023-02-09 23:51:56 -0300

    feat: neovide hides mouse while typing

--- lua/abrolhus/neovide.lua
+++ lua/abrolhus/neovide.lua
@@ -8,12 +8,12 @@ if vim.g.neovide then
     vim.g.neovide_floating_blur_amount_y = 2.0
     vim.g.neovide_transparency = 0.97
     vim.g.neovide_scroll_animation_length = 0.3
-    vim.g.neovide_hide_mouse_when_typing = false
+    vim.g.neovide_hide_mouse_when_typing = true
     vim.g.neovide_underline_automatic_scaling = false
     vim.g.neovide_refresh_rate = 60
     vim.g.neovide_refresh_rate_idle = 5
     vim.g.neovide_no_idle = true
-    vim.g.neovide_confirm_quit = true
+    vim.g.neovide_confirm_quit = false
     vim.g.neovide_fullscreen = false
     vim.g.neovide_remember_window_size = true
     vim.g.neovide_cursor_animation_length = 0.05
