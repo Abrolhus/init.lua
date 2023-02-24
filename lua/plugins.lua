c51e75027a5f53d6047f3b2faacdc9a4dedd8b76
Samuel Abreu <54046223+Abrolhus@users.noreply.github.com>
2023-02-24 16:29:59 -0300

    barbecue winbar

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -68,6 +68,19 @@ return {
         -- dependencies = { "kyazdani42/nvim-web-devicons", lazy = true }
         dependencies = { "nvim-tree/nvim-web-devicons" }
     },
+    --- winbar:
+    {
+        "utilyre/barbecue.nvim",
+        name = "barbecue",
+        version = "*",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons", -- optional dependency
+        },
+        opts = {
+            -- configurations go here
+        },
+    },
     --- tab line
     { "akinsho/bufferline.nvim",
         version = "v3.*",
