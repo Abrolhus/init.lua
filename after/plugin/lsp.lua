b291c08bee0cf5ee923076e667f110aa23c6c576
Samuel Abreu <54046223+Abrolhus@users.noreply.github.com>
2023-02-24 15:57:57 -0300

    better lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -37,7 +37,7 @@ lsp.nvim_workspace()
 
 lsp.on_attach(function(client, bufnr)
     local navic = require("nvim-navic")
-    local opts = { buffer = bufnr, remap = false }
+    -- local opts = { buffer = bufnr, remap = false }
 
     if client.server_capabilities.documentSymbolProvider then
         navic.attach(client, bufnr)
@@ -49,21 +49,21 @@ lsp.on_attach(function(client, bufnr)
         }
     }, bufnr)
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-    vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
-    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>ev", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "<leader>en", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "<leader>ep", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>ef", function() vim.lsp.buf.code_action({ only = "quickfix" }) end) -- diagnostic fix
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "<leader>sf", function() vim.lsp.buf.format() end, opts) -- format entire buffer
-    vim.keymap.set("n", "<leader>sa", function() vim.lsp.buf.code_action() end, opts) -- code action!
-    vim.keymap.set("n", "<leader>sr", function() vim.lsp.buf.rename() end, opts) -- rename symbol
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, { buffer = bufnr, remap = false, desc = "Go to Definition" })
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, { buffer = bufnr, remap = false, desc = "Go to References" })
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, { buffer = bufnr, remap = false, desc = "Go to Implementation" })
+    vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, { buffer = bufnr, remap = false, desc = "Go to Type definition" })
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, { buffer = bufnr, remap = false, desc = "Go to Declaration" })
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, { buffer = bufnr, remap = false, desc = "Hover" }) -- similar to 'K' in help menus
+    -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, {buffer=bufnr, remap = false, desc = "})
+    vim.keymap.set("n", "<leader>ev", function() vim.diagnostic.open_float() end, { buffer = bufnr, remap = false, desc = "Error View" })
+    vim.keymap.set("n", "<leader>en", function() vim.diagnostic.goto_next() end, { buffer = bufnr, remap = false, desc = "Error Next" })
+    vim.keymap.set("n", "<leader>ep", function() vim.diagnostic.goto_prev() end, { buffer = bufnr, remap = false, desc = "Error Prev" })
+    vim.keymap.set("n", "<leader>ef", function() vim.lsp.buf.code_action({ only = "quickfix" }) end, {desc = "Error Fix"}) -- diagnostic fix
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, { buffer = bufnr, remap = false, desc = "signature Help" })
+    vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, { buffer = bufnr, remap = false, desc = "Lsp Format"}) -- format entire buffer
+    vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, { buffer = bufnr, remap = false, desc = "Lsp Action" }) -- code action!
+    vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, { buffer = bufnr, remap = false, desc = "Lsp Rename" }) -- rename symbol
 end)
 
 lsp.setup()
